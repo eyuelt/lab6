0c87b7448e9ffeaadf7a16110530fbc7be194800
eyuelt <eyueltessema@gmail.com>
2014-02-14 02:39:23 -0800

    Use ajax to populate data for projects

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -2,37 +2,49 @@
 
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
-	initializePage();
+  initializePage();
 })
 
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
-	$('.project a').click(addProjectDetails);
+  $('.project a').click(addProjectDetails);
 
-	$('#colorBtn').click(randomizeColors);
+  $('#colorBtn').click(randomizeColors);
 }
 
 /*
  * Make an AJAX call to retrieve project details and add it in
  */
 function addProjectDetails(e) {
-	// Prevent following the link
-	e.preventDefault();
+  // Prevent following the link
+  e.preventDefault();
 
-	// Get the div ID, e.g., "project3"
-	var projectID = $(this).closest('.project').attr('id');
-	// get rid of 'project' from the front of the id 'project3'
-	var idNumber = projectID.substr('project'.length);
+  // Get the div ID, e.g., "project3"
+  var projectID = $(this).closest('.project').attr('id');
+  // get rid of 'project' from the front of the id 'project3'
+  var idNumber = projectID.substr('project'.length);
 
-	console.log("User clicked on project " + idNumber);
+  console.log("User clicked on project " + idNumber);
+
+  $.get("/project/"+idNumber, populateData);
+
+  function populateData(response) {
+    console.log(response);
+    var projectHTML = '' +
+      '<img class="detailsImage" src="' + response.image + '" />' +
+      '<p><small>' + response.date + '</small></p>' +
+      response.summary;
+    $('#project'+idNumber+' .details').html(projectHTML);
+  };
 }
+
 
 /*
  * Make an AJAX call to retrieve a color palette for the site
  * and apply it
  */
 function randomizeColors(e) {
-	console.log("User clicked on color button");
+  console.log("User clicked on color button");
 }
