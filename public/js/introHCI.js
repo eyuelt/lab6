75870861cc4e6ea44f9550f22fa97a0d7b909117
eyuelt <eyueltessema@gmail.com>
2014-02-14 02:48:16 -0800

    Randomize colors

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -47,4 +47,15 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
   console.log("User clicked on color button");
+
+  $.get("/palette", function(result) {
+    console.log('randomizing colors...')
+    var colors = result.colors.hex;
+  $('body').css('background-color', colors[0]);
+  $('.thumbnail').css('background-color', colors[1]);
+  $('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+  $('p').css('color', colors[3]);
+  $('.project img').css('opacity', .75);
+
+  });
 }
